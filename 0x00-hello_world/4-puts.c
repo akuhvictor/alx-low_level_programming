ca0e3511d3862d239919639a4485b632376f18fc
akuhvictor <ojonugwav2001@gmail.com>
2023-09-07 23:27:22 +0000

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - the main function
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);                               }
-        
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
