6aa26774ddd7b1b1d16447ff1ed27cfb50539e98
akuhvictor <ojonugwav2001@gmail.com>
2023-09-08 00:50:44 +0000

    Quote

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,7 @@
 #include <unistd.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 1 (Error)
- */
-int main(void)
-{
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, msg, sizeof(msg) - 1);
-
-	return (1);
+int main(void) {
+    const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, 59);
+    return 1;
 }
