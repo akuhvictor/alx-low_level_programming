881bda0dab7589f96103999f49e4b578fac5bee8
akuhvictor <ojonugwav2001@gmail.com>
2023-09-08 01:19:32 +0000

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,13 @@
 #include <unistd.h>
 
 /**
- * main - Entry point of the program
+ * main - Entry point
  *
- * Return: Always 1 (indicating an error)
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-ssize_t len = 59;
-
-ssize_t wr = write(2, msg, len);
-if (wr != len)
-  return (1);
-
-  return (1);
+    char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, msg, sizeof(msg) - 1);
+    return (1);
 }
