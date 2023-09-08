e07b8e251c111802c861c37dd505b91f78f4c77c
akuhvictor <ojonugwav2001@gmail.com>
2023-09-08 01:04:33 +0000

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,14 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, 59);
+    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, msg, sizeof(msg) - 1);
+
     return (1);
 }
