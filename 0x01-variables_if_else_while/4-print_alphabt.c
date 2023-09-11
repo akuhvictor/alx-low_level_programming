afd23c9d1c612b58e56e077277731a87199425e0
akuhvictor <ojonugwav2001@gmail.com>
2023-09-11 22:01:11 +0000

    Print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: Function prints alphabets in lowercase 
- * except letter q and e 
+ * Description: Function prints alphabets in lowercase
+ * except letter q and e
  * Return: Always 0 (Success)
  */
 int main(void)
