a6ee22d9e3f7fbf1fed4eedb67ae0d366f2d221b
akuhvictor <ojonugwav2001@gmail.com>
2023-09-11 22:22:17 +0000

    Print numberz

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: This program prints all the numbers of base 16 in lowercase,
+ * followed by a new line, using only three putchar calls.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int i;
+char hex_digits[] = "0123456789abcdef";
+int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        putchar('0' + i);
-    }
+for (i = 0; i < 16; i++)
+{
+putchar(hex_digits[i]);
+}
 
-    for (i = 0; i < 6; i++)
-    {
-        putchar('a' + i);
-    }
+putchar('\n');
 
-    putchar("\n");
-
-    return 0;
+return (0);
 }
