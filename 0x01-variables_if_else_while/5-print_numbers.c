23706a0d1e071fc5d988ad71ed98971218c1b55c
akuhvictor <ojonugwav2001@gmail.com>
2023-09-11 22:03:59 +0000

    Print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Function to print all sigle digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
- int i;
+int i;
 
- for (i = 0; i <= 9; i++)
- {
-  putchar(i + '0');
- }
+for (i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+}
 
- putchar('\n');
+putchar('\n');
 
- return 0;
+return (0);
 }
