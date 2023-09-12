daafc26a27e58ef314f97d10dfded3e6e321f896
akuhvictor <ojonugwav2001@gmail.com>
2023-09-12 22:39:30 +0000

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,24 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Description: This program prints putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+_putchar('_');
+_putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-    return (0);
+return (0);
 }
