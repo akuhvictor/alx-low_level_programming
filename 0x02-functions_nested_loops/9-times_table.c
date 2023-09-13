c309de58a87a0429d1b1e90d28cdf2dcd8233a8e
akuhvictor <ojonugwav2001@gmail.com>
2023-09-13 00:00:57 +0000

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,29 +13,24 @@ for (column = 0; column <= 9; column++)
 {
 product = row * column;
 
-if (column == 0)
+if (column != 0)
 {
-_putchar('0');
-}
-else
-{
+_putchar(',');
 _putchar(' ');
+}
+
 if (product < 10)
 {
 _putchar(' ');
-}
-}
-
 _putchar(product + '0');
-            
-if (column < 9)
-{
-_putchar(',');
 }
 else
 {
+_putchar((product / 10) + '0');
+_putchar((product % 10) + '0');
+}
+}
 _putchar('\n');
 }
 }
-}
-}
+
