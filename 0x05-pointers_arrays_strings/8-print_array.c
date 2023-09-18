3aa22f4ab0466900270f8a18464adf9e6c86483f
akuhvictor <ojonugwav2001@gmail.com>
2023-09-18 16:12:04 +0000

    Print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,7 +8,7 @@
 void print_array(int *a, int n)
 {
 if (n <= 0)
-{void print_array(int *a, int n)
+{
 putchar('\n');
 return;
 }
@@ -16,7 +16,6 @@ return;
 for (int i = 0; i < n; i++)
 {
 printf("%d", a[i]);
-
 
 if (i < n - 1)
 {
