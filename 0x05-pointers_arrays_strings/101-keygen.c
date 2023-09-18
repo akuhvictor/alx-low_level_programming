8cab23bcb566e3750e721c7bd842921379a9bc30
akuhvictor <ojonugwav2001@gmail.com>
2023-09-18 23:28:16 +0000

    Keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char getRandomChar(const char *charSet, int charSetSize) {
+    return charSet[rand() % charSetSize];
+}
+
+void generateRandomPassword(char *password, int length) {
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    int charsetSize = sizeof(charset) - 1;
+
+    for (int i = 0; i < length; i++) {
+        password[i] = getRandomChar(charset, charsetSize);
+    }
+
+    password[length] = '\0';
+}
+
+int main() {
+    srand(time(NULL));
+
+    int passwordLength = 8;
+
+    char password[passwordLength + 1];
+
+    generateRandomPassword(password, passwordLength);
+
+    printf("Generated Password: %s\n", password);
+
+    return 0;
+}
