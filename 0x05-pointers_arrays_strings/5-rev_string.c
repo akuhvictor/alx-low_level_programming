486c8975a5c7b3532379ad7c6823777c3daafa0b
akuhvictor <ojonugwav2001@gmail.com>
2023-09-18 15:24:39 +0000

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse followed by a newline to stdout.
+ * @s: Pointer to the string to be printed in reverse.
+ */
+void print_rev(char *s)
+{
+int length = 0;
+
+while (s[length] != '\0')
+{
+length++;
+}
+
+
+for (int i = length - 1; i >= 0; i--)
+{
+putchar(s[i]);
+}
+
+putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,6 @@
 #define MAIN_H
 
 
-void _puts(char *str);
+void print_rev(char *s);
 
 #endif
