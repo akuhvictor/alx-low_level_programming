d736c76710c97d71a1e5bdbaca75f7ca8b2487c5
akuhvictor <ojonugwav2001@gmail.com>
2023-09-18 15:06:27 +0000

    Swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -9,7 +9,7 @@ void swap_int(int *a, int *b)
 {
 int temp;
 
-temp = *a;  // Store the value of 'a' in a temporary variable.
-*a = *b;    // Assign the value of 'b' to 'a'.
-*b = temp;  // Assign the temporary value to 'b'.
+temp = *a;
+*a = *b;
+*b = temp;
 }
