9f34fefe3759f776c8650b413681d3704178484e
akuhvictor <ojonugwav2001@gmail.com>
2023-09-18 15:04:32 +0000

    Swap

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * swap_int - Swaps the values of two integers.
- * @a: Pointer to the first integer.
- * @b: Pointer to the second integer.
- */
+
 void swap_int(int *a, int *b);
 
 #endif
